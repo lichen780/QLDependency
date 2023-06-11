c72559d8480aeb05fb6a0f3e97fb2a3c5df72fe9
lichen <39560327+lichen780@users.noreply.github.com>
2023-06-11 14:50:25 +0800

    Update XinQLOneKey.sh

--- Shell/XinQLOneKey.sh
+++ Shell/XinQLOneKey.sh
@@ -56,7 +56,7 @@ npm install -g date-fns axios jsdom ts-md5 ds crypto-js
 pip3 install ping3 requests pycryptodome aiohttp cython utils
 pip3 install requests
 
-#cd /ql/data/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
+#cd /ql/data/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js jsdom ts-md5 ds crypto-js canvas --build-from-source
 #cd /ql
 #apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd /ql/data/scripts/ && npm install canvas --build-from-source
 #cd /ql
